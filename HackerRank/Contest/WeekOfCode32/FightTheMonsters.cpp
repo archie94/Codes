3f9d9e422768e815d3c85a4e124982eb19fe1e9e
Arka Prava Basu <arkaprava94@gmail.com>
2017-05-24 23:43:24 +0530

    Solved from editorial

--- HackerRank/Contest/WeekOfCode32/FightTheMonsters.cpp
+++ HackerRank/Contest/WeekOfCode32/FightTheMonsters.cpp
@@ -11,13 +11,20 @@ int main() {
 	for(int i=0;i<n;i++) cin>>h[i];
 
 	sort(h,h+n);
-	int k=0;
-	for(long long i=1;i<=t;i++) {
-		h[k]-=hit;
-		if(h[k]<=0) {
-			k++;
+	long long k=0;
+    	long long i=0;
+    	long long ans=0;
+	for(;i<n;i++) {
+		long long rt=h[i]/hit;
+       	 	if(rt*hit<h[i]) {
+            		++rt;
+        	}
+        	t-=rt;
+		if(t<0) {
+			break;
 		}
+		ans++;
 	}
-	cout<<k<<endl;
+	cout<<ans<<endl;
 	return 0;
 }
