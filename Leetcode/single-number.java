c9fb7becc917b72f20af50071676f3390e36c9a3
Arka Prava Basu <arkaprava94@gmail.com>
2018-03-17 11:05:56 +0530

    Solved

---
+++ Leetcode/single-number.java
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/single-number/description/
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = nums[0];
+        for (int i =1; i < nums.length; i++) {
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+}
