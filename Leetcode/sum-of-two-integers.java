7a8e6a1f515047594599c6dba96e6279ffecb4be
Arka Prava Basu <arkaprava94@gmail.com>
2018-03-18 23:43:58 +0530

    Solved

---
+++ Leetcode/sum-of-two-integers.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/sum-of-two-integers/description/
+
+class Solution {
+    public int getSum(int a, int b) {
+        
+        if (a == 0) return b;
+        if (b == 0) return a;
+        
+        int carry = a & b;
+        
+        while (b != 0) {
+            carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        
+        return a;
+    }
+}
