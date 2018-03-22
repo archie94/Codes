f3a22547d4052dc7798497c7ad694ef12b625f86
Arka Prava Basu <arkaprava94@gmail.com>
2018-03-22 09:30:51 +0530

    Solved

---
+++ Leetcode/kth-largest-element-in-array.java
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        
+        return nums[nums.length - k];
+    }
+}
